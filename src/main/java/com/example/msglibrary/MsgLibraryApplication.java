498aeab751a9454a618ddaf22d9e5173a95b9d0c
Enayatullah Aria <55733496+Enayatullah-aria@users.noreply.github.com>
2022-04-01 13:43:43 +0200

    Initial commit V1. Projekt ist bereit erstellt und mit PostreSQl verbunden

---
+++ src/main/java/com/example/msglibrary/MsgLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.msglibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsgLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/msglibrary/MsgLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msglibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
